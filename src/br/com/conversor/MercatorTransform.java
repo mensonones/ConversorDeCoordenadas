bd2282b7776819536886fe2531f382de2b717f01
crowley626 <crowley626@c3po>
2016-03-20 14:45:14 -0300

    Renomeaçao do pacote.

--- src/br/com/conversor/MercatorTransform.java
+++ src/br/com/conversor/MercatorTransform.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.clycia;
+package br.com.conversor;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
--- src/br/com/conversor/XYTOLATLONGI.java
+++ src/br/com/conversor/XYTOLATLONGI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.clycia;
+package br.com.conversor;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
